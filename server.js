055bf1d9de7e5114c221af88201c46a0c87ce3e8
manjunath <manju.paramesan@medialinksaustralia.com.au>
2018-04-30 06:50:47 +1000

    Setup start script and heruko deploy

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-var app = express();
+let port = process.env.PORT || 3000;
+let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 app.use((req, res, next) => {
-  var now = new Date().toString();
-  var log = `${now}: ${req.method} ${req.url}`;
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
 
   console.log(log);
   fs.appendFile('server.log', log + '\n', err => {
@@ -48,6 +49,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
